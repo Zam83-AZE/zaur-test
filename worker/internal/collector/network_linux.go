a6f054d759162cd6597c386c60cf809383e97282
SysWorker CI <ci@sysworker.dev>
2026-04-10 20:55:15 +0000

    fix: remove unused path/filepath import from network_linux.go

--- worker/internal/collector/network_linux.go
+++ worker/internal/collector/network_linux.go
@@ -3,81 +3,80 @@
 package collector
 
 import (
-	"net"
-	"os"
-	"path/filepath"
-	"strings"
+        "net"
+        "os"
+        "strings"
 
-	"github.com/Zam83-AZE/zaur-test/worker/internal/models"
+        "github.com/Zam83-AZE/zaur-test/worker/internal/models"
 )
 
 // Docker/container interface patterns
 var virtualPrefixes = []string{
-	"br-",    // Docker bridge
-	"veth",   // Docker veth
-	"docker", // Docker
-	"virbr",  // Virtual bridge (libvirt)
-	"vnet",   // libvirt
+        "br-",    // Docker bridge
+        "veth",   // Docker veth
+        "docker", // Docker
+        "virbr",  // Virtual bridge (libvirt)
+        "vnet",   // libvirt
 }
 
 func CollectNetwork() []models.NetworkInterface {
-	var interfaces []models.NetworkInterface
+        var interfaces []models.NetworkInterface
 
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		return interfaces
-	}
+        ifaces, err := net.Interfaces()
+        if err != nil {
+                return interfaces
+        }
 
-	for _, iface := range ifaces {
-		// Skip loopback
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue
-		}
+        for _, iface := range ifaces {
+                // Skip loopback
+                if iface.Flags&net.FlagLoopback != 0 {
+                        continue
+                }
 
-		ni := models.NetworkInterface{
-			Name:       iface.Name,
-			MACAddress: iface.HardwareAddr.String(),
-		}
+                ni := models.NetworkInterface{
+                        Name:       iface.Name,
+                        MACAddress: iface.HardwareAddr.String(),
+                }
 
-		// Get IP addresses
-		addrs, err := iface.Addrs()
-		if err == nil {
-			for _, addr := range addrs {
-				if ipnet, ok := addr.(*net.IPNet); ok {
-					if !ipnet.IP.IsLoopback() && !ipnet.IP.IsLinkLocalUnicast() {
-						ni.IPAddresses = append(ni.IPAddresses, ipnet.IP.String())
-					}
-				}
-			}
-		}
+                // Get IP addresses
+                addrs, err := iface.Addrs()
+                if err == nil {
+                        for _, addr := range addrs {
+                                if ipnet, ok := addr.(*net.IPNet); ok {
+                                        if !ipnet.IP.IsLoopback() && !ipnet.IP.IsLinkLocalUnicast() {
+                                                ni.IPAddresses = append(ni.IPAddresses, ipnet.IP.String())
+                                        }
+                                }
+                        }
+                }
 
-		// Determine interface type
-		ni.InterfaceType = "Unknown"
+                // Determine interface type
+                ni.InterfaceType = "Unknown"
 
-		// Check for WiFi
-		if _, err := os.Stat("/sys/class/net/" + iface.Name + "/wireless"); err == nil {
-			ni.InterfaceType = "WiFi"
-		} else {
-			// Check for virtual/container interfaces
-			isVirtual := false
-			for _, prefix := range virtualPrefixes {
-				if strings.HasPrefix(iface.Name, prefix) {
-					isVirtual = true
-					break
-				}
-			}
-			if isVirtual {
-				ni.InterfaceType = "Virtual"
-			} else {
-				ni.InterfaceType = "Ethernet"
-			}
-		}
+                // Check for WiFi
+                if _, err := os.Stat("/sys/class/net/" + iface.Name + "/wireless"); err == nil {
+                        ni.InterfaceType = "WiFi"
+                } else {
+                        // Check for virtual/container interfaces
+                        isVirtual := false
+                        for _, prefix := range virtualPrefixes {
+                                if strings.HasPrefix(iface.Name, prefix) {
+                                        isVirtual = true
+                                        break
+                                }
+                        }
+                        if isVirtual {
+                                ni.InterfaceType = "Virtual"
+                        } else {
+                                ni.InterfaceType = "Ethernet"
+                        }
+                }
 
-		// Skip interfaces with no useful info
-		if len(ni.IPAddresses) > 0 || ni.MACAddress != "" {
-			interfaces = append(interfaces, ni)
-		}
-	}
+                // Skip interfaces with no useful info
+                if len(ni.IPAddresses) > 0 || ni.MACAddress != "" {
+                        interfaces = append(interfaces, ni)
+                }
+        }
 
-	return interfaces
+        return interfaces
 }
